5d0a5f2880effa407f5569d0b805c9211ef48ea3
Rebeca Farias <fariasrebecac@gmail.com>
2020-08-31 22:16:15 -0300

    validando campos obrigatorios

--- backend/src/config/database.js
+++ backend/src/config/database.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb://localhost27017/todo'
+const url = 'mongodb://localhost:27017/todo'
 
 mongoose.connect(url, { useNewUrlParser: true});
 
